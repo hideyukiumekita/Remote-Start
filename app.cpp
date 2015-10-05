a49af9ead72a1fa8c144dba6d66b66b67ed53c8b
hideyukiumekita <vhideyuki.umekita@gmail.com>
2015-10-05 11:36:11 +0900

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -4,8 +4,9 @@
  *  Implementation of the Task main_task
  *  Author: Kazuhiro.Kawachi
  *  Copyright (c) 2015 Embedded Technology Software Design Robot Contest
- *  #001 リモートスタート
- *  #002 タッチセンサスタート
+ *  Revision
+ *  #001 Bluetooth対応
+ *  #002 タッチセンサ対応
  *  #003 尻尾制御
  *****************************************************************************/
 
@@ -41,7 +42,7 @@ static Balancer        *gBalancer;
 static BalancingWalker *gBalancingWalker;
 static LineTracer      *gLineTracer;
 
-/* オブジェクトへのポインタ定義 */                                              //#001
+// オブジェクトへのポインタ定義
 Clock*        clock;                                                            //#001
 TouchSensor*  touchSensor;                                                      //#002
 Motor*        tailMotor;                                                        //#003
@@ -58,9 +59,9 @@ static void user_system_create() {
                                            gBalancer);
     gLineMonitor     = new LineMonitor(gColorSensor);
     gLineTracer      = new LineTracer(gLineMonitor, gBalancingWalker);
-	clock            = new Clock();                                             //#001
-	touchSensor      = new TouchSensor(PORT_1);                                 //#002
-	tailMotor        = new Motor(PORT_A);                                       //#003
+	clock            = new Clock();                                         //#001
+	touchSensor      = new TouchSensor(PORT_1);                             //#002
+	tailMotor        = new Motor(PORT_A);                                   //#003
 
     // 初期化完了通知
     ev3_led_set_color(LED_ORANGE);
@@ -79,30 +80,35 @@ static void user_system_destroy() {
     delete gBalancer;
 }
 
-/**                                                                             //#003
- * tail_control                                                                 //#003
- */                                                                             //#003
+/**
+ * tail_control
+ */
 static void tail_control(int32_t angle)                                         //#003
 {                                                                               //#003
-    float pwm = (float)(angle - tailMotor->getCount()) * P_GAIN; /* 比例制御 */ //#003
-	                                                                              //#003
-    /* PWM出力飽和処理 */                                                       //#003
-    if (pwm > PWM_ABS_MAX)                                                      //#003
-    {                                                                           //#003
-        pwm = PWM_ABS_MAX;                                                      //#003
-    }                                                                           //#003
-    else if (pwm < -PWM_ABS_MAX)                                                //#003
-    {                                                                           //#003
-        pwm = -PWM_ABS_MAX;                                                     //#003
-    }                                                                           //#003
-    tailMotor->setPWM(pwm);                                                     //#003
+	// 比例制御
+	float pwm = (float)(angle - tailMotor->getCount()) * P_GAIN;            //#003
+	
+	// PWM出力飽和処理
+	if (pwm > PWM_ABS_MAX)                                                  //#003
+	{                                                                       //#003
+		pwm = PWM_ABS_MAX;                                              //#003
+	}                                                                       //#003
+	else if (pwm < -PWM_ABS_MAX)                                            //#003
+	{                                                                       //#003
+		pwm = -PWM_ABS_MAX;                                             //#003
+	}                                                                       //#003
+	tailMotor->setPWM(pwm);                                                 //#003
+	
 }                                                                               //#003
 
 /*                                                                              //#001
  *  Bluetooth                                                                   //#001
- */                                                                             //#001
-static int32_t   bt_cmd = 0;      /* Bluetoothコマンド 1:リモートスタート */    //#001
-static FILE     *bt = NULL;      /* Bluetoothファイルハンドル */                //#001
+ */
+ // Bluetoothコマンド 1:リモートスタート
+static int32_t   bt_cmd = 0;
+
+// Bluetoothファイルハンドル
+static FILE     *bt = NULL;
 
 /*                                                                              //#003
  *  tail                                                                        //#003
@@ -116,37 +122,42 @@ void ev3_cyc_tracer(intptr_t exinf) {
     act_tsk(TRACER_TASK);
 }
 
-
 /**
  * メインタスク
  */
 void main_task(intptr_t unused)
 {
-    user_system_create();  // センサやモータの初期化処理
+	// センサやモータの初期化処理
+	user_system_create();
 	
-	/* 尻尾モーターのリセット */                                                //#003
-    tailMotor->reset();                                                       //#003
-
-    // 周期ハンドラ開始
-    ev3_sta_cyc(EV3_CYC_TRACER);
-
-    slp_tsk();  // バックボタンが押されるまで待つ
-
-    // 周期ハンドラ停止
-    ev3_stp_cyc(EV3_CYC_TRACER);
-
-    user_system_destroy();  // 終了処理
-
-    ext_tsk();
+	// 尻尾モーターのリセット
+	tailMotor->reset();                                                     //#003
+	
+	// 周期ハンドラ開始
+	ev3_sta_cyc(EV3_CYC_TRACER);
+	
+	// バックボタンが押されるまで待つ
+	slp_tsk();
+	
+	// 周期ハンドラ停止
+	ev3_stp_cyc(EV3_CYC_TRACER);
+	
+	// 終了処理
+	user_system_destroy();
+	
+	ext_tsk();
+	
 }
 
 
-/**                                                                             //#001
- * BTタスク                                                                     //#001
- */                                                                             //#001
-void bt_task(intptr_t unused){                                                  //#001
-    while(1){                                                                   //#001
-        uint8_t c = fgetc(bt); /* 受信 */                                       //#001
+/**
+ * BTタスク
+ */
+void bt_task(intptr_t unused)                                                   //#001
+{                                                                               //#001
+	while(1){                                                               //#001
+    	// 受信
+        uint8_t c = fgetc(bt);                                                  //#001
         switch(c){                                                              //#001
         case '1':                                                               //#001
             bt_cmd = 1;                                                         //#001
@@ -154,9 +165,10 @@ void bt_task(intptr_t unused){                                                  
         default:                                                                //#001
             break;                                                              //#001
         }                                                                       //#001
-        fputc(c, bt); /* エコーバック */                                        //#001
+        // エコーバック
+        fputc(c, bt);                                                           //#001
     }                                                                           //#001
-	ext_tsk();                                                                    //#001
+	ext_tsk();                                                              //#001
 }                                                                               //#001
 
 
@@ -165,45 +177,48 @@ void bt_task(intptr_t unused){                                                  
  */
 void tracer_task(intptr_t exinf)
 {
-	/* Open Bluetooth file */                                                     //#001
-    bt = ev3_serial_open_file(EV3_SERIAL_BT);                                   //#001
-    assert(bt != NULL);                                                         //#001
-
-    /* Bluetooth通信タスクの起動 */                                             //#001
-    act_tsk(BT_TASK);                                                           //#001
-	                                                                              //#001
-	/* スタート待機 */                                                            //#001
-	while(1)                                                                      //#001
-	{                                                                             //#001
-		tail_control(TAIL_ANGLE_STAND_UP); /* 完全停止用角度に制御 */               //#003
+	// Open Bluetooth file
+	bt = ev3_serial_open_file(EV3_SERIAL_BT);                               //#001
+	assert(bt != NULL);                                                     //#001
+	
+	// Bluetooth通信タスクの起動
+	act_tsk(BT_TASK);                                                       //#001
+	
+	// スタート待機
+	while(1)                                                                //#001
+	{                                                                       //#001
+		// 完全停止用角度に制御
+		tail_control(TAIL_ANGLE_STAND_UP);                              //#003
 		
-		if(bt_cmd == 1)                                                             //#001
-		{                                                                           //#001
-			break;                                                                    //#001
-		}                                                                           //#001
-		if(touchSensor->isPressed())                                                //#002
-		{                                                                           //#002
-			break;                                                                    //#002
-		}                                                                           //#002
-		clock->sleep(10);                                                           //#001
-	}                                                                             //#001
+		if(bt_cmd == 1)                                                 //#001
+		{                                                               //#001
+			break;                                                  //#001
+		}                                                               //#001
+		if(touchSensor->isPressed())                                    //#002
+		{                                                               //#002
+			break;                                                  //#002
+		}                                                               //#002
+		clock->sleep(10);                                               //#001
+	}                                                                       //#001
 	
-	
-	while(1)                                                                      //#003
-	{                                                                             //#003
+	while(1)                                                                //#003
+	{                                                                       //#003
 		if (ev3_button_is_pressed(BACK_BUTTON))
 		{
-			wup_tsk(MAIN_TASK);  // バックボタン押下
+			// バックボタン押下
+			wup_tsk(MAIN_TASK);
 		}
+		// バランス走行用角度に制御
+		tail_control(TAIL_ANGLE_DRIVE);                                 //#003
 		
-		tail_control(TAIL_ANGLE_DRIVE); /* バランス走行用角度に制御 */              //#003
+		// 倒立走行
+		gLineTracer->run();
 		
-		gLineTracer->run(); // 倒立走行
-		
-		clock->sleep(4); /* 4msec周期起動 */                                        //#003
+		// 4msec周期起動
+		clock->sleep(4);                                                //#003
 	}
-	ter_tsk(BT_TASK);                                                             //#001
-	fclose(bt);                                                                   //#001
+	ter_tsk(BT_TASK);                                                       //#001
+	fclose(bt);                                                             //#001
 	ext_tsk();
 }
 }
